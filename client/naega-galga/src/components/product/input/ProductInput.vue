e8773d9e39f2b06d166152b0320fd190ed2d8d45
jaehe <jaehe0413@naver.com>
2023-01-16 21:51:21 +0900

    add : ProductInput, image input

---
+++ client/naega-galga/src/components/product/input/ProductInput.vue
@@ -0,0 +1,12 @@
+<template>
+  <product-image-input></product-image-input>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+import ProductImageInput from "@/components/product/input/ProductImageInput.vue";
+
+export default defineComponent({
+  components: { ProductImageInput },
+});
+</script>
