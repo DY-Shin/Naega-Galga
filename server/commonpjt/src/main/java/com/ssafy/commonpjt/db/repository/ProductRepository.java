1244a29c97155f23518dbd6c1ec9f141e4028f69
이흥종 <avjg123@naver.com>
2023-01-16 11:07:19 +0900

    Delete ProductRepository.java

--- server/commonpjt/src/main/java/com/ssafy/commonpjt/db/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.commonpjt.db.repository;
-
-import com.ssafy.commonpjt.db.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-    public String findById(int id);
-}
