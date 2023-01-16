475bc8c0371a6e5bc7f87734d5b1fa26f7132b37
jun <ktf1406@naver.com>
2023-01-16 12:31:11 +0900

    server first commit

---
+++ server/commonpjt/src/test/java/com/ssafy/commonpjt/NaegaGalgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.commonpjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaegaGalgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
