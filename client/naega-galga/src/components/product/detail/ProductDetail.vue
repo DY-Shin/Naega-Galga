5de20e04877d72b58acd32e4828beb0de484515d
박재희 <jaehe0413@naver.com>
2023-01-16 15:23:26 +0900

    feature : 78-매물 상세보기 화면 작성

---
+++ client/naega-galga/src/components/product/detail/ProductDetail.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="image-list">
+    <product-image-list></product-image-list>
+  </div>
+  <div>
+    <right-summary-box :summary-value="summaryValue"></right-summary-box>
+    <h2>매물 정보</h2>
+    <product-info :info="product"></product-info>
+    <h2>옵션</h2>
+    <product-option-list :options="product.options"></product-option-list>
+  </div>
+</template>
+
+<script lang="ts">
+import ProductImageList from "@/components/product/detail/ProductImageList.vue";
+import ProductOptionList from "@/components/product/detail/ProductOptionList.vue";
+import RightSummaryBox from "@/components/product/detail/RightSummaryBox.vue";
+import ProductInfo from "@/components/product/detail/ProductInfo.vue";
+import { computed } from "@vue/runtime-core";
+
+export default {
+  components: {
+    ProductImageList,
+    ProductOptionList,
+    RightSummaryBox,
+    ProductInfo,
+  },
+  setup() {
+    interface Product {
+      productType: string;
+      productName: string;
+      price: string;
+      managePrice: number;
+      floor: string;
+      roomSize: number;
+      address: string;
+      roomDirection: string;
+      animal: boolean;
+      seller?: string;
+      explanationDate: string | null;
+      parking: number;
+      options: Array<string>;
+    }
+    const product: Product = {
+      productType: "월세",
+      productName: "싸피빌라",
+      price: "1000/30",
+      managePrice: 5,
+      floor: "10층/3층",
+      roomSize: 29.5,
+      address: "경상북도 구미시 진평동 13",
+      roomDirection: "남향",
+      animal: false,
+      seller: "싸피부동산",
+      explanationDate: "2023.1.30",
+      parking: 0,
+      options: ["에어컨", "냉장고"],
+    };
+
+    const summaryValue = computed(() => ({
+      productType: product.productType,
+      price: product.price,
+      floor: product.floor,
+      managePrice: product.managePrice,
+      seller: product.seller,
+      explanationDate: product.explanationDate,
+    }));
+
+    return {
+      product,
+      summaryValue,
+    };
+  },
+};
+</script>
+
+<style scoped>
+div > h2:nth-child(2) {
+  margin-top: 10px !important;
+}
+div > h2:nth-child(4) {
+  margin-top: 40px !important;
+}
+
+.image-list {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ client/naega-galga/src/components/product/detail/ProductInfo.vue
@@ -0,0 +1,92 @@
+<template>
+  <el-row>
+    <el-col :span="4">{{ productInfo?.productType }}</el-col>
+    <el-col :span="20">{{ productInfo?.price }}</el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="4">관리비</el-col>
+    <el-col :span="20"> {{ productInfo?.managePrice }}</el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="4">층수</el-col>
+    <el-col :span="20">
+      {{ productInfo?.floor }}
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="4">면적</el-col>
+    <el-col :span="20"> {{ productInfo.roomSize }}m<sup>2</sup> </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="4">건물 이름</el-col>
+    <el-col :span="20"> {{ productInfo.productName }}</el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="4">상세주소</el-col>
+    <el-col :span="20">
+      {{ productInfo.address }}
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="4">방향</el-col>
+    <el-col :span="20">
+      {{ productInfo.roomDirection }}
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="4">반려동물</el-col>
+    <el-col :span="20">
+      {{ productInfo.animal }}
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="4">주차</el-col>
+    <el-col :span="20">{{ productInfo.parking }}</el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="4">판매자</el-col>
+    <el-col :span="20">{{ productInfo.seller }}</el-col>
+  </el-row>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  props: {
+    info: {
+      type: Object,
+      productType: String,
+      productName: String,
+      price: String,
+      managePrice: Number,
+      floor: Number,
+      roomSize: Number,
+      address: String,
+      roomDirection: String,
+      parking: [Number, String],
+      animal: Boolean,
+      seller: String,
+    },
+  },
+  setup(props) {
+    const productInfo = { ...props.info };
+    const booleanToString = (value: boolean): string =>
+      value ? "가능" : "불가능";
+
+    productInfo.parking =
+      productInfo.parking === 0 ? "불가능" : productInfo.parking;
+    productInfo.parking = booleanToString(productInfo.parking);
+    productInfo.animal = booleanToString(productInfo.animal);
+
+    return { productInfo };
+  },
+});
+</script>
+
+<style scoped>
+.el-row {
+  font-size: var(--el-font-size-large);
+  margin-top: 20px;
+}
+</style>
