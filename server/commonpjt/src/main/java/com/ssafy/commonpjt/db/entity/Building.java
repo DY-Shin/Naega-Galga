6ba663c835fdb49ba149d357a3c44943a29bdc02
jun <ktf1406@naver.com>
2023-01-17 17:48:55 +0900

    create : Entity And Repo

---
+++ server/commonpjt/src/main/java/com/ssafy/commonpjt/db/entity/Building.java
@@ -0,0 +1,33 @@
+//package com.ssafy.commonpjt.db.entity;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "building")
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+//public class Building {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name="building_index", nullable = false)
+//    private Integer buildingIndex;
+//
+//    @Column(nullable = false)
+//    private String buildingAddress;
+//
+//    @Column(nullable = false)
+//    private String buildingName;
+//
+//    @Column(nullable = false)
+//    private String buildingElevator;
+//
+//    @Column(nullable = false)
+//    private String buildingParking;
+//}
---
+++ server/commonpjt/src/main/java/com/ssafy/commonpjt/db/repository/BuildingRepository.java
@@ -0,0 +1,10 @@
+//package com.ssafy.commonpjt.db.repository;
+//
+//import com.ssafy.commonpjt.db.entity.Building;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface BuildingRepository extends JpaRepository<Building, Integer> {
+//    void findAllByBuildingAddress();
+//    void findBuildingByBuilding();
+//
+//}
