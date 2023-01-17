3464f545c491486756898de6697aebe73b8f9760
오민준 <sige_tank@naver.com>
2023-01-17 16:56:23 +0900

    feat : 회원가입 페이지 구성

--- client/naega-galga/src/router/index.ts
+++ client/naega-galga/src/router/index.ts
@@ -2,13 +2,15 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import userRouter from "@/router/userRouter";
 import productRouter from "@/router/productRouter";
 
+const joinRouter = userRouter[1];
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     component: import("@/views/HomeView.vue"),
   },
   userRouter[0],
-  userRouter[1],
+  joinRouter,
   productRouter,
 ];
 
