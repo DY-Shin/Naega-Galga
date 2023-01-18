e6f927b25789e000388db4ec38473df118628a20
jun <ktf1406@naver.com>
2023-01-18 17:43:30 +0900

    create : Error Files

--- server/commonpjt/src/main/java/com/ssafy/commonpjt/common/exception/CustomException.java
+++ server/commonpjt/src/main/java/com/ssafy/commonpjt/common/exception/CustomException.java
@@ -1,4 +1,10 @@
 package com.ssafy.commonpjt.common.exception;
 
-public class CustomException {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException{
+    ErrorCode errorCode;
 }
