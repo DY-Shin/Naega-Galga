1d8fffae58f3ed365367aba05f6ae5394d6ff93b
박재희 <jaehe0413@naver.com>
2023-01-21 15:15:20 +0900

    edit/78-매물 상세보기 화면 수정

--- client/naega-galga/src/components/product/detail/ProductInfo.vue
+++ client/naega-galga/src/components/product/detail/ProductInfo.vue
@@ -2,46 +2,66 @@
   <table>
     <tbody>
       <tr>
-        <td width="150px" class="text-bold text-large">
+        <td width="200px" class="text-bold text-large">
           {{ productInfo?.productType }}
         </td>
         <td class="text-large">{{ productInfo?.price }}</td>
       </tr>
       <tr>
-        <td width="150px" class="text-bold text-large">관리비</td>
+        <td width="200px" class="text-bold text-large">관리비</td>
         <td class="text-large">{{ productInfo?.managePrice }}</td>
       </tr>
       <tr>
-        <td width="150px" class="text-bold text-large">층수</td>
-        <td class="text-large">{{ productInfo?.floor }}</td>
-      </tr>
-      <tr>
-        <td width="150px" class="text-bold text-large">면적</td>
+        <td width="200px" class="text-bold text-large">면적</td>
         <td class="text-large">{{ productInfo.roomSize }}m<sup>2</sup></td>
       </tr>
       <tr>
-        <td width="150px" class="text-bold text-large">건물 이름</td>
-        <td class="text-large">{{ productInfo.productName }}</td>
+        <td width="200px" class="text-bold text-large">방 방향</td>
+        <td class="text-large">{{ productInfo.roomDirection }}향</td>
       </tr>
       <tr>
-        <td width="150px" class="text-bold text-large">상세주소</td>
-        <td class="text-large">{{ productInfo.address }}</td>
+        <td width="200px" class="text-bold text-large">층수</td>
+        <td class="text-large">{{ productInfo?.floor }}</td>
       </tr>
       <tr>
-        <td width="150px" class="text-bold text-large">방향</td>
-        <td class="text-large">{{ productInfo.roomDirection }}</td>
+        <td width="200px" class="text-bold text-large">방 종류</td>
+        <td class="text-large">{{ productInfo?.roomType }}</td>
       </tr>
       <tr>
-        <td width="150px" class="text-bold text-large">반려동물</td>
-        <td class="text-large">{{ productInfo.animal }}</td>
-      </tr>
-      <tr>
-        <td width="150px" class="text-bold text-large">주차</td>
+        <td width="200px" class="text-bold text-large">주차</td>
         <td class="text-large">{{ productInfo.parking }}</td>
       </tr>
       <tr>
-        <td width="150px" class="text-bold text-large">판매자</td>
-        <td class="text-large">{{ productInfo.seller }}</td>
+        <td width="200px" class="text-bold text-large">반려동물</td>
+        <td class="text-large">{{ productInfo.animal }}</td>
+      </tr>
+      <tr>
+        <td width="200px" class="text-bold text-large">엘리베이터</td>
+        <td class="text-large">{{ productInfo.elevator }}</td>
+      </tr>
+      <tr>
+        <td width="200px" class="text-bold text-large">판매자</td>
+        <td class="text-large">{{ productInfo.sellerId }}</td>
+      </tr>
+      <tr>
+        <td width="200px" class="text-bold text-large">주소</td>
+        <td class="text-large">
+          <tr>
+            {{
+              productInfo.roadAddress
+            }}
+          </tr>
+          <tr>
+            {{
+              productInfo.jibunAddress
+            }}
+          </tr>
+          <tr>
+            {{
+              `${productInfo.buildingName} ${productInfo.roomHo}`
+            }}
+          </tr>
+        </td>
       </tr>
     </tbody>
   </table>
@@ -54,21 +74,28 @@ export default defineComponent({
   props: {
     info: {
       type: Object,
-      productType: String,
-      productName: String,
-      price: String,
-      managePrice: Number,
-      floor: Number,
-      roomSize: Number,
-      address: String,
-      roomDirection: String,
-      parking: [Number, String],
-      animal: Boolean,
-      seller: String,
+      value: {
+        productType: String,
+        price: String,
+        managePrice: Number,
+        roomSize: Number,
+        roomDirection: String,
+        floor: Number,
+        roomType: String,
+        parking: [Number, String],
+        animal: Boolean,
+        elevator: Boolean,
+        roadAddress: String,
+        jibunAddress: String,
+        buildingName: String,
+        roomHo: String,
+        sellerId: String,
+      },
     },
   },
   setup(props) {
     const productInfo = { ...props.info };
+    productInfo.managePrice = `월 ${productInfo.managePrice}만`;
     const booleanToString = (value: boolean): string =>
       value ? "가능" : "불가능";
 
@@ -76,6 +103,7 @@ export default defineComponent({
       productInfo.parking === 0 ? "불가능" : productInfo.parking;
     productInfo.parking = booleanToString(productInfo.parking);
     productInfo.animal = booleanToString(productInfo.animal);
+    productInfo.elevator = booleanToString(productInfo.elevator);
 
     return { productInfo };
   },
@@ -84,7 +112,7 @@ export default defineComponent({
 
 <style scoped>
 .text-bold {
-  font-weight: bold;
+  font-weight: 600;
 }
 
 .text-large {
