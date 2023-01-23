954f5b24f686b8d9dde417d11c30a7f91cf71ff6
jaehe <jaehe0413@naver.com>
2023-01-23 20:34:39 +0900

    feat : alert 컴포넌트 생성

---
+++ client/naega-galga/src/components/common/AlertComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <el-alert
+      v-if="getIsShow"
+      show-icon
+      :type="getAlertInfo.alertType"
+      :title="getAlertInfo.title"
+      :description="getAlertInfo.description"
+      @close="setIsShow(false)"
+    >
+    </el-alert>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+const getIsShow = computed(() => store.getters["alertStore/getIsShow"]);
+const getAlertInfo = computed(() => store.getters["alertStore/getAlertInfo"]);
+
+const setIsShow = (value: boolean) =>
+  store.dispatch("alertStore/SET_IS_SHOW", value);
+</script>
+
+<style scoped>
+div {
+  position: fixed;
+  right: 0;
+  width: 300px;
+  max-width: 300px;
+}
+.el-alert {
+  width: 400px;
+}
+</style>
