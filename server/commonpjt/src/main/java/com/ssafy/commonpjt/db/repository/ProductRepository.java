0326799493b6b67521bb025724b228a9f141fd93
jun <ktf1406@naver.com>
2023-01-25 17:56:41 +0900

    edit : Repo Files

--- server/commonpjt/src/main/java/com/ssafy/commonpjt/db/repository/ProductRepository.java
+++ server/commonpjt/src/main/java/com/ssafy/commonpjt/db/repository/ProductRepository.java
@@ -1,12 +1,20 @@
 package com.ssafy.commonpjt.db.repository;
 
 import com.ssafy.commonpjt.db.entity.Product;
+import com.ssafy.commonpjt.db.entity.Building;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-    List<Product> findProductBy();
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+//    매물 찾기
+    List<Product> findAll();
+    @Query("select p " +
+            "from Product p join fetch p.building b join fetch p.options op join fetch p.productSeller s " +
+            "where p.building.buildingIndex = ?1")
+    List<Product> productFetchJoin(Integer buildingIndex);
+
+//    List<Product> findByBuildingAnd
 }
