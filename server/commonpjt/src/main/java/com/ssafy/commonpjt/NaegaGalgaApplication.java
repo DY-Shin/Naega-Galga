53beb1e7611420b377b9e830aa6e250e652c9c98
이준 <ktf1406@naver.com>
2023-01-26 15:41:14 +0900

    feat : 32 - 카카오 지도 검색 API 요청처리 / : 64 - 매물 검색 요청처리 / : 37 - 매물 상세 정보 요청처리

--- server/commonpjt/src/main/java/com/ssafy/commonpjt/NaegaGalgaApplication.java
+++ server/commonpjt/src/main/java/com/ssafy/commonpjt/NaegaGalgaApplication.java
@@ -2,8 +2,9 @@ package com.ssafy.commonpjt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class NaegaGalgaApplication {
 
 	public static void main(String[] args) {
