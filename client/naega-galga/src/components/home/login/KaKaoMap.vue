8079d78ab9361465b5040b6f22b38b94ae99a8f0
dnjs0236 <dnjs0236@gmail.com>
2023-01-26 09:19:24 +0900

    edit : 카카오맵 리스트 함수 수정

--- client/naega-galga/src/components/home/login/KaKaoMap.vue
+++ client/naega-galga/src/components/home/login/KaKaoMap.vue
@@ -9,7 +9,7 @@
 </style>
 
 <script lang="ts">
-import { defineComponent, onMounted, watch, PropType } from "@vue/runtime-core";
+import { defineComponent, onMounted, watch } from "@vue/runtime-core";
 
 declare global {
   interface Window {
@@ -21,30 +21,27 @@ declare global {
 export default defineComponent({
   props: {
     GetAddress: { type: String },
-    GetList: { type: Array as PropType<Array<object>> },
+    GetList: { type: Array },
   },
 
   setup(props) {
+    // let markers: number[] = [];
     watch(
       () => props.GetAddress,
       () => {
-        console.log("getAddress!!");
         changeCenter(props.GetAddress);
+        console.log(props.GetList?.length + "!!!!!!!!!!");
       }
     );
-
     watch(
       () => props.GetList,
       () => {
-        console.log("getList!! " + props.GetList?.length);
-        displayMarker(props.GetList);
-        // window.map.setCenter(
-        //   new window.kakao.maps.LatLng(33.450705, 126.570677)
-        // );
-      }
+        console.log("getList!! ");
+        // displayMarker(window.map, props.GetList);
+      },
+      { deep: true }
     );
 
-    // let markers: [number[]];
     const makeOverListener = (map, marker, infowindow) =>
       function () {
         infowindow.open(map, marker);
@@ -55,12 +52,20 @@ export default defineComponent({
       };
 
     const markerPositions1: object[] = [
-      { title: "카카오", latlng: [33.450705, 126.570677] },
-      { title: "생태연못", latlng: [33.450936, 126.569477] },
+      {
+        address: "부산 동래구 충렬대로 255",
+        a: 35.2014786272255,
+        b: 129.087166169007,
+      },
+      {
+        address: "경상북도 구미시 인동6길 26-2",
+        a: 36.1020372425131,
+        b: 128.420294611527,
+      },
     ];
 
-    const latitude = 33.450705;
-    const longitude = 126.570677;
+    const latitude = 36.1020372425131;
+    const longitude = 128.420294611527;
     onMounted(() => {
       const mapScript = document.createElement("script");
       mapScript.async = true;
@@ -78,60 +83,57 @@ export default defineComponent({
           center: new window.kakao.maps.LatLng(latitude, longitude),
         };
         window.map = new window.kakao.maps.Map(container, options);
+        // displayMarker(markerPositions1);
+        displayMarker2(markerPositions1);
       });
-      console.log(changeCenter("경상북도 구미시 인동6길 26 - 2"));
     };
 
-    const displayMarker = markerList => {
+    // const displayMarker = markerList => {
+    //   let bounds = new window.kakao.maps.LatLngBounds();
+    //   if (markerList.length > 0) {
+    //     // markers.forEach(marker => marker.setMap(null));
+    //   }
+    //   for (let i = 0; i < markerList.length; i++) {
+    //     let coords = new window.kakao.maps.LatLng(
+    //       markerList[i].a,
+    //       markerList[i].b
+    //     );
+    //     console.log("1 " + coords);
+    //     let marker = new window.kakao.maps.Marker({
+    //       position: coords,
+    //     });
+    //     // markers.push([markerList[i].a, markerList[i].b]);
+    //     marker.setMap(window.map);
+    //     bounds.extend(coords);
+    //   }
+    //   console.log("1!!! " + bounds);
+    //   window.map.setBounds(bounds);
+    // };
+    const displayMarker2 = markerList => {
       let geocoder = new window.kakao.maps.services.Geocoder();
-
-      if (markerList.length > 0) {
-        // markers.forEach(marker => marker.setMap(null));
-      }
       let bounds = new window.kakao.maps.LatLngBounds();
-      if (markerList.length > 0) {
-        for (let i = 0; i < markerList.length; i++) {
-          geocoder.addressSearch(
-            markerList[i].address,
-            function (result, status) {
-              if (status === window.kakao.maps.services.Status.OK) {
-                let coords = new window.kakao.maps.LatLng(
-                  result[0].y,
-                  result[0].x
-                );
-                let marker = new window.kakao.maps.Marker({ position: coords });
-                marker.setMap(window.map);
-                bounds.extend(coords);
-                let infowindow = new window.kakao.maps.InfoWindow({
-                  content:
-                    '<div class="" style="width: 150px; height: 100px; text- align: center; padding: 10px 0; border: 1px solid red"> asd < /div>',
-                  removable: true,
-                });
-                window.kakao.maps.event.addListener(
-                  marker,
-                  "mouseover",
-                  makeOverListener(window.map, marker, infowindow)
-                );
-                window.kakao.maps.event.addListener(
-                  marker,
-                  "mouseout",
-                  makeOutListener(infowindow)
-                );
-              }
-            }
-          );
-        }
-        window.map.setBounds(bounds);
+      for (let i = 0; i < markerList.length; i++) {
+        let callback = function (result, status) {
+          if (status === window.kakao.maps.services.Status.OK) {
+            let coords = window.kakao.maps.LatLng(result[0].y, result[0].x);
+            let marker = new window.kakao.maps.Marker({
+              position: coords,
+            });
+            marker.setMap(window.map);
+            bounds.extend(coords);
+          }
+        };
+
+        geocoder.addressSearch("해남군 송지면", callback);
       }
     };
+
     const changeCenter = address => {
       let geocoder = new window.kakao.maps.services.Geocoder();
       geocoder.addressSearch(address, function (result, status) {
         if (status === window.kakao.maps.services.Status.OK) {
-          let coords = new window.kakao.maps.LatLng(
-            Number(result[0].y),
-            Number(result[0].x)
-          );
+          let coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+          console.log("3 " + coords);
           window.map.setCenter(coords);
           window.map.setLevel(1);
           let marker = new window.kakao.maps.Marker({
