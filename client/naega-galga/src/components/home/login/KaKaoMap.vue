768870f688f54a7364de0a2603da1810acfd62d7
dnjs0236 <dnjs0236@gmail.com>
2023-01-27 16:44:25 +0900

    feat : 인포윈도우 생성

--- client/naega-galga/src/components/home/login/KaKaoMap.vue
+++ client/naega-galga/src/components/home/login/KaKaoMap.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="test">
+  <!-- <div class="test">
     <div style="">부산 동래구 충렬대로 255</div>
     <div style="">싸피원룸</div>
     <div style="">부산 동래구 충렬대로 255</div>
-  </div>
+  </div> -->
   <el-container><el-main id="map"></el-main></el-container>
 </template>
 
@@ -25,23 +25,27 @@ declare global {
 
 export default defineComponent({
   props: {
-    GetAddress: { type: String },
+    GetIdx: { type: Number },
     GetList: { type: Array },
   },
 
   setup(props) {
     // let markers: number[] = [];
     watch(
-      () => props.GetAddress,
+      () => props.GetIdx,
       () => {
-        changeCenter(props.GetAddress);
+        console.log(props.GetIdx);
+        changeCenter(props.GetIdx);
       }
     );
     watch(
       () => props.GetList,
       () => {
         displayMarker(window.map, props.GetList);
-        console.log(infowindows.length);
+        // console.log(props.GetList);
+        // console.log(markers[0].getPosition);
+        // console.log(markers[1].getPosition);
+        // console.log(markers[2].getPosition);
         for (let i = 0; i < infowindows.length; i++) {
           infowindows[i].close();
         }
@@ -52,7 +56,6 @@ export default defineComponent({
     const clickListener = (map, marker, infowindow) =>
       function () {
         infowindow.open(map, marker);
-        infowindows.push(infowindow);
       };
 
     const markerPositions1: object[] = [
@@ -87,17 +90,15 @@ export default defineComponent({
           center: new window.kakao.maps.LatLng(latitude, longitude),
         };
         window.map = new window.kakao.maps.Map(container, options);
-        // displayMarker(window.map, markerPositions1);
       });
     };
-    const setBounds = bounds => {
-      window.map.setBounds(bounds);
-    };
-    let infowindows: any[] = [];
 
+    let infowindows: any[] = [];
     let markers: any[] = [];
+    let nums: any[] = [];
 
     const displayMarker = (map, markerList) => {
+      // 매물 목록 검색 결과 마커 표시함
       if (markers.length > 0) {
         for (let i = 0; i < markers.length; i++) {
           markers[i].setMap(null);
@@ -116,10 +117,11 @@ export default defineComponent({
                 result[0].y,
                 result[0].x
               );
+              nums[i] = num;
               let imageSrc =
-                  "https://cdn-icons-png.flaticon.com/512/2776/2776067.png",
-                imageSize = new window.kakao.maps.Size(50, 50),
-                imageOption = { offset: new window.kakao.maps.Point(25, 45) };
+                  "https://cdn-icons-png.flaticon.com/512/7976/7976202.png",
+                imageSize = new window.kakao.maps.Size(40, 40),
+                imageOption = { offset: new window.kakao.maps.Point(20, 40) };
               let markerImage = new window.kakao.maps.MarkerImage(
                   imageSrc,
                   imageSize,
@@ -127,17 +129,17 @@ export default defineComponent({
                 ),
                 markerPosition = coords;
 
-              // 결과값으로 받은 위치를 마커로 표시합니다
               let marker = new window.kakao.maps.Marker({
                 map: map,
                 position: markerPosition,
                 image: markerImage,
               });
 
-              setInfoWindow(coords, marker, "set");
+              setInfoWindow(coords, marker, markerList[i]); // 상세 정보 창 만들어주고
 
               marker.setMap(map);
-              markers.push(marker);
+              markers[num] = marker;
+
               bounds.extend(coords);
             }
             num++;
@@ -148,46 +150,66 @@ export default defineComponent({
         );
       }
     };
-
-    const changeCenter = address => {
-      let geocoder = new window.kakao.maps.services.Geocoder();
-      geocoder.addressSearch(address, function (result, status) {
-        if (status === window.kakao.maps.services.Status.OK) {
-          let coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
-          window.map.setCenter(coords);
-          window.map.setLevel(1);
-          let marker = new window.kakao.maps.Marker({
-            position: coords,
-          });
-          marker.setMap(window.map);
-          markers.push(marker);
-
-          setInfoWindow(coords, marker, "click");
-        }
-      });
+    const setBounds = bounds => {
+      // 모든 마커 범위 포함하게 지도 범위 재설정
+      window.map.setBounds(bounds);
     };
-
-    const setInfoWindow = (coords, marker, type) => {
+    const setInfoWindow = (coords, marker, product) => {
+      // 상세 정보 창 만들어서 배열에 넣음 클릭 시 열림 이벤트 등록
+      // const move = () => {
+      //   console.log("!!");
+      //   alert("asd");
+      // };
+      // const move = () => {
+      //   console.log("!!");
+      //   alert("!!!");
+      // };
       let infowindow = new window.kakao.maps.InfoWindow({
-        content:
-          '<div  style="width: 250px; height: 100px; text- align: center; padding: 10px 0; border: 1px solid red"> asd < /div>',
+        content: `<button @click="${console.log(
+          "!!"
+        )}">내가갈가[家]</button><div style="padding:5px;">${product.address} ${
+          product.type
+        } <br><a href="/" style="color:blue" target="_blank">${
+          product.address
+        }</a> <a href="https://map.kakao.com/link/to/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">길찾기</a></div>`,
+        // content: `<div
+        // style="
+        // width: 300px;
+        // height: 100px;
+        // text-align: center;
+        // padding: 10px 0;
+        // border: 1px solid white">${product.address} ${product.type}
+        // <button style="border:none;
+        // background: whitesmoke;
+        // cursor:pointer"
+        // @click="location.href='/'">hi</button>`,
         removable: true,
       });
+
       window.kakao.maps.event.addListener(
         marker,
         "click",
         clickListener(window.map, marker, infowindow)
       );
-      console.log(type);
-      if (type == "click") {
-        infowindow.open(
-          window.map,
-          new window.kakao.maps.Marker({
-            position: coords,
-          })
-        );
-        infowindows.push(infowindow);
-      }
+      infowindows.push(infowindow);
+    };
+
+    const changeCenter = addr_idx => {
+      console.log(nums);
+      // 목록에서 선택 시 해당 위치로 지도 중심 이동, 상세 정보 창 열림
+      let idx = nums[addr_idx];
+      let marker = markers[idx];
+      let coords = marker.getPosition();
+
+      window.map.setCenter(coords);
+      window.map.setLevel(1);
+      infowindows[idx].open(
+        window.map,
+        new window.kakao.maps.Marker({
+          position: coords,
+        })
+      );
+      // setInfoWindow(marker.getPosition(), marker);
     };
 
     return {
--- client/naega-galga/src/components/home/login/LoginHome.vue
+++ client/naega-galga/src/components/home/login/LoginHome.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <main-side @address="sendAddress" @productList="sendList"></main-side>
+      <main-side @addr_idx="sendIdx" @productList="sendList"></main-side>
       <el-container :style="{ height: 'calc(100vh - 150px)' }">
-        <ka-kao-map
-          :GetAddress="addressProxy"
-          :GetList="listProxy"
-        ></ka-kao-map>
+        <ka-kao-map :GetIdx="idxProxy" :GetList="listProxy"></ka-kao-map>
       </el-container>
     </el-container>
     <chat-icon></chat-icon>
@@ -24,42 +21,25 @@ export default defineComponent({
   components: { MainSide, KaKaoMap, ChatIcon },
 
   setup() {
-    const addressProxy = ref("");
-    const sendAddress = (address: string) => {
-      addressProxy.value = address;
+    const idxProxy = ref();
+    const sendIdx = (sendIdx: number) => {
+      idxProxy.value = sendIdx;
     };
-
     let listProxy = ref([]);
     const sendList = (list: []) => {
       listProxy.value.splice(0);
       for (let i = 0; i < list.length; i++) {
         listProxy.value.push(list[i]);
       }
+
       for (let i = 0; i < list.length; i++) {
-        console.log("listProxy length! " + listProxy.value.length);
+        console.log("listProxy " + listProxy[i]);
       }
     };
 
-    // interface Product {
-    //   productType: string;
-    //   productName: string;
-    //   price: string;
-    //   managePrice: number;
-    //   floor: string;
-    //   roomSize: number;
-    //   address: string;
-    //   roomDirection: string;
-    //   animal: boolean;
-    //   seller?: string;
-    //   explanationDate: string | null;
-    //   parking: number;
-    //   options: Array<string>;
-    //   isFavorite: boolean;
-    // }
-
     return {
-      sendAddress,
-      addressProxy,
+      sendIdx,
+      idxProxy,
       sendList,
       listProxy,
     };
