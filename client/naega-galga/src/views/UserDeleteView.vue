f126e8ea208c8dd4aa6fbe52a660f1b885ebef72
오민준 <sige_tank@naver.com>
2023-02-01 16:07:43 +0900

    feat : getters에 필요한 내용 추가

--- client/naega-galga/src/views/UserDeleteView.vue
+++ client/naega-galga/src/views/UserDeleteView.vue
@@ -3,18 +3,19 @@
   <el-input v-model="passwordForm.password" placeholder="" />
   <button @click="checkPassword">비밀번호 확인</button>
   <!-- 정말로 삭제 할지 한번 묻고 진짜 회원탈퇴 진행 -->
+  <button @click="userdelete">탈퇴버튼</button>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref } from "vue";
-import { useStore } from "vuex";
 import { useRouter } from "vue-router";
 import apiTokenInstance from "@/api/apiTokenInstance";
+import apiInstance from "@/api/apiInstance";
+import localStorageManager from "@/utils/localStorageManager";
 
 export default defineComponent({
   name: "UserDeleteView",
   setup() {
-    const store = useStore();
     const router = useRouter();
 
     const passwordForm = reactive({
@@ -38,8 +39,19 @@ export default defineComponent({
     };
 
     const userdelete = () => {
-      store.dispatch("userStore/userDelete");
-      router.push({ path: "" });
+      apiInstance
+        .post("/api/users/delete", {
+          accessToken: localStorageManager.getAccessToken(),
+          refreshToken: localStorageManager.getRefreshToken(),
+          checkPassword: passwordForm.password,
+        })
+        .then(res => {
+          console.log(res);
+          router.push({ path: "" });
+        })
+        .catch(err => {
+          console.log(err);
+        });
     };
 
     return { passwordForm, checkPassword, userdelete, isChecked };
