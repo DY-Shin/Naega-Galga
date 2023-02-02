870d8fda44ecb3d317496a93e0fc61b192e67f6f
dnjs0236 <dnjs0236@gmail.com>
2023-02-02 15:15:49 +0900

    edit : 지도에서 문의하기 눌러서 채팅방 열었을 때 닫고 다시 열면 없어지는 거 수정

--- client/naega-galga/src/api/chatApi.ts
+++ client/naega-galga/src/api/chatApi.ts
@@ -19,4 +19,14 @@ async function getChatContent(chatRoomIndex: number) {
   return response;
 }
 
-export { getChatRooms, getChatContent };
+async function getChatRoomInfo(OpIndex: number) {
+  const response: AxiosResponse = await apiTokenInstance.post(
+    `/api/chat/rooms/ask`,
+    {
+      OpIndex: OpIndex,
+    }
+  );
+  return response;
+}
+
+export { getChatRooms, getChatContent, getChatRoomInfo };
