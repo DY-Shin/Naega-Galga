482b3ae4079fca2c30238f6fddc72a84e5eb64b1
kavin816@naver.com <kavin816@naver.com>
2023-02-02 16:41:28 +0900

    0202

--- server/commonpjt/src/test/java/com/ssafy/commonpjt/SearchTests.java
+++ server/commonpjt/src/test/java/com/ssafy/commonpjt/SearchTests.java
@@ -1,48 +1,48 @@
-//package com.ssafy.commonpjt;
-//
-//import com.ssafy.commonpjt.api.service.SearchServiceImpl;
-//import com.ssafy.commonpjt.db.entity.Product;
-//import com.ssafy.commonpjt.db.repository.BuildingRepository;
-//import com.ssafy.commonpjt.db.repository.ProductRepository;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-////@SpringBootTest
-//class SearchTests {
-//
-//    @Autowired
-//    private ProductRepository productRepository;
-//
-//    @Autowired
-//    private BuildingRepository buildingRepository;
-//
-//    @Autowired
-//    private SearchServiceImpl searchService;
-//
-//    @Test
-//    void contextLoads() {
-//
-////        List<Building> buildings = buildingRepository.findAll();
-////        List<Product> products = productRepository.findAll();
-//        // List<?> list = searchService.searchProduct("구미시 송정동");
-//        List<Integer> buildingIndex = buildingRepository.findBuildingIndexByBuildingAddressStartingWithAndBuildingRoadAddressStartingWith("경북 구미시 송정동","");
-//
-//        for (Integer idx : buildingIndex) {
-//            List<Product> product = new ArrayList<>();
-//            System.out.println(idx);
-//            product = productRepository.productFetchJoin(idx);
-//
-//            for(Product p : product) {
-//                System.out.println(p.getProductIndex());
-//                System.out.println(p.getProductPrice());
-//                System.out.println(p.getBuilding().getBuildingRoadAddress());
-//                System.out.println(p.getOptions().isOptionBed());
-//            }
-//        }
-//    }
-//
-//}
+package com.ssafy.commonpjt;
+
+import com.ssafy.commonpjt.api.service.SearchServiceImpl;
+import com.ssafy.commonpjt.db.entity.Product;
+import com.ssafy.commonpjt.db.repository.BuildingRepository;
+import com.ssafy.commonpjt.db.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+class SearchTests {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private BuildingRepository buildingRepository;
+
+    @Autowired
+    private SearchServiceImpl searchService;
+
+    @Test
+    void contextLoads() {
+
+//        List<Building> buildings = buildingRepository.findAll();
+//        List<Product> products = productRepository.findAll();
+        // List<?> list = searchService.searchProduct("구미시 송정동");
+        List<Integer> buildingIndex = buildingRepository.findBuildingIndexByBuildingAddressStartingWithAndBuildingRoadAddressStartingWith("경북 구미시 송정동","");
+
+        for (Integer idx : buildingIndex) {
+            List<Product> product = new ArrayList<>();
+            System.out.println(idx);
+            product = productRepository.productFetchJoin(idx);
+
+            for(Product p : product) {
+                System.out.println(p.getProductIndex());
+                System.out.println(p.getProductPrice());
+                System.out.println(p.getBuilding().getBuildingRoadAddress());
+                System.out.println(p.getOptions().isOptionBed());
+            }
+        }
+    }
+
+}
