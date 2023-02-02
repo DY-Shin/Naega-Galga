02c764e489e604e8a62703c83779b24f093e6cb9
kavin816@naver.com <kavin816@naver.com>
2023-02-02 16:34:27 +0900

    0202 AWS test

--- server/commonpjt/src/test/java/com/ssafy/commonpjt/SearchTests.java
+++ server/commonpjt/src/test/java/com/ssafy/commonpjt/SearchTests.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.ArrayList;
 import java.util.List;
 
-@SpringBootTest
+//@SpringBootTest
 class SearchTests {
 
     @Autowired
