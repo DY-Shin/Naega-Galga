f93b5c83adbfa9affc85faa9dd7574429182a2ed
백소원 <dnjs0236@gmail.com>
2023-02-02 17:04:29 +0900

    Feat/147 채팅 목록 요청

--- client/naega-galga/src/api/chatApi.ts
+++ client/naega-galga/src/api/chatApi.ts
@@ -2,10 +2,9 @@ import { AxiosResponse } from "axios";
 //TODO : 나중에 token instance로 바꿀것
 import apiTokenInstance from "./apiInstance";
 
-async function getChatRooms(userIndex: number) {
-  const response: AxiosResponse = await apiTokenInstance.get(
-    `/api/chat/rooms/${userIndex}`
-  );
+async function getChatRooms() {
+  console.log("getRooms");
+  const response: AxiosResponse = await apiTokenInstance.get(`/api/chat/rooms`);
   return response;
 }
 
