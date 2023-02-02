dafe9af982c318a142d080e453735128a11c4040
박재희 <jaehe0413@naver.com>
2023-02-02 22:02:29 +0900

    edit : 128-일대일 화상 미팅방 입장

---
+++ client/naega-galga/src/components/meeting/one-on-one/OvVideo.vue
@@ -0,0 +1,19 @@
+<template>
+  <video autoplay ref="videoElRef"></video>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+
+export default {
+  props: {
+    streamManager: Object,
+  },
+
+  setup(props) {
+    const videoElRef = ref(null);
+
+    props.streamManager?.addVideoElement(videoElRef);
+  },
+};
+</script>
