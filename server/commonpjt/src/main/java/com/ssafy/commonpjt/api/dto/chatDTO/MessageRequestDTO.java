8ae1db14bebab698ba0e1ae4ee36c80c70d9e409
jun <ktf1406@naver.com>
2023-02-03 12:53:42 +0900

    edit : Test Files

--- server/commonpjt/src/main/java/com/ssafy/commonpjt/api/dto/chatDTO/MessageRequestDTO.java
+++ server/commonpjt/src/main/java/com/ssafy/commonpjt/api/dto/chatDTO/MessageRequestDTO.java
@@ -1,5 +1,17 @@
 package com.ssafy.commonpjt.api.dto.chatDTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MessageRequestDTO {
 
 }
