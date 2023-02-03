dca42d28270e5f16578bb78010017931d8260ee0
jaehe <jaehe0413@naver.com>
2023-02-03 21:41:15 +0900

    feat : 일대일 화상 미팅 모바일 화면

--- client/naega-galga/src/use/useMediaQuery.ts
+++ client/naega-galga/src/use/useMediaQuery.ts
@@ -1,5 +1,5 @@
 import { useMediaQuery } from "@vueuse/core";
 
-const isMobileScreen = useMediaQuery("(min-width: 500px)");
+const isMobileScreen = useMediaQuery("(max-width: 500px)");
 
 export { isMobileScreen };
