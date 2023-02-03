89892ddc70e6e57f0973c95b9e34eff59b89309e
jun <ktf1406@naver.com>
2023-02-03 17:08:20 +0900

    edit : Chat Service Files

--- server/commonpjt/src/main/java/com/ssafy/commonpjt/api/service/ChatServiceImpl.java
+++ server/commonpjt/src/main/java/com/ssafy/commonpjt/api/service/ChatServiceImpl.java
@@ -42,4 +42,6 @@ public class ChatServiceImpl implements ChatService{
 
         return result;
     }
+
+
 }
---
+++ server/commonpjt/src/main/java/com/ssafy/commonpjt/api/service/NotificationSerivce.java
@@ -0,0 +1,7 @@
+package com.ssafy.commonpjt.api.service;
+
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+public interface NotificationSerivce {
+
+}
