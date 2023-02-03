e2b66f1d255e57b5d01377beb7d58800620bee60
jun <ktf1406@naver.com>
2023-02-03 17:44:32 +0900

    delete : Import Files

--- server/commonpjt/src/test/java/com/ssafy/commonpjt/ChatTest.java
+++ server/commonpjt/src/test/java/com/ssafy/commonpjt/ChatTest.java
@@ -1,14 +1,8 @@
 package com.ssafy.commonpjt;
 
-import com.ssafy.commonpjt.api.dto.chatDTO.ChatRoomResponseDTO;
-import com.ssafy.commonpjt.api.service.ChatService;
 import com.ssafy.commonpjt.api.service.ChatServiceImpl;
-import com.ssafy.commonpjt.api.service.SearchServiceImpl;
 import com.ssafy.commonpjt.db.entity.ChatRoom;
-import com.ssafy.commonpjt.db.entity.Product;
-import com.ssafy.commonpjt.db.repository.BuildingRepository;
 import com.ssafy.commonpjt.db.repository.ChatRoomRepository;
-import com.ssafy.commonpjt.db.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
