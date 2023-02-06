e4fe1e17802c16c0f04c010fb778adb0879ad76d
kavin816@naver.com <kavin816@naver.com>
2023-02-06 18:01:12 +0900

    0206

--- client/naega-galga/src/api/meetingApi.ts
+++ client/naega-galga/src/api/meetingApi.ts
@@ -1,10 +1,7 @@
-// import apiTokenInstance from "./apiTokenInstance";
 import axios from "axios";
 
 const getOneOnOneMeetingInfo = async (meetingIndex: number) => {
-  const response = await axios.get(
-    `api/meeting/one-on-one/${meetingIndex}`
-  );
+  const response = await axios.get(`api/meeting/one-on-one/${meetingIndex}`);
   return response;
 };
 
