284c363263f2c2c2ed3d97f33a0b586bd3908754
오민준 <sige_tank@naver.com>
2023-02-08 13:12:06 +0900

    fix : meeting api 수정

--- client/naega-galga/src/api/meetingApi.ts
+++ client/naega-galga/src/api/meetingApi.ts
@@ -1,13 +1,23 @@
 import axios from "axios";
+import localStorageManager from "@/utils/localStorageManager";
 
 const getOneOnOneMeetingInfo = async (meetingIndex: number) => {
-  const response = await axios.get(`api/meeting/one-on-one/${meetingIndex}`);
+  const response = await axios.get(`api/meeting/one-on-one/${meetingIndex}`, {
+    headers: {
+      Authorization: `Bearer ${localStorageManager.getAccessToken()}`,
+    },
+  });
   return response;
 };
 
 const getOneOnManyMeetingInfo = async (explanationIndex: number) => {
   const response = await axios.get(
-    `api/meeting/one-on-many/${explanationIndex}`
+    `api/meeting/one-on-many/${explanationIndex}`,
+    {
+      headers: {
+        Authorization: `Bearer ${localStorageManager.getAccessToken()}`,
+      },
+    }
   );
   return response;
 };
