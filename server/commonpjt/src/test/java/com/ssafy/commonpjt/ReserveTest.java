98a753e1a5acb2116f2320659ae53b194047efde
jun <ktf1406@naver.com>
2023-02-08 14:30:31 +0900

    edit : Reserve Test File

--- server/commonpjt/src/test/java/com/ssafy/commonpjt/ReserveTest.java
+++ server/commonpjt/src/test/java/com/ssafy/commonpjt/ReserveTest.java
@@ -2,22 +2,40 @@ package com.ssafy.commonpjt;
 
 import com.ssafy.commonpjt.api.dto.reserveDTO.ReserveRequestDTO;
 import com.ssafy.commonpjt.api.service.ReserveServiceImpl;
+import com.ssafy.commonpjt.db.entity.User;
+import com.ssafy.commonpjt.db.repository.MeetingRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 @SpringBootTest
 public class ReserveTest {
 
     @Autowired
     private ReserveServiceImpl reserveService;
+    @Autowired
+    private MeetingRepository meetingRepository;
+
     @Test
     void test() {
-        ReserveRequestDTO requestDTO = ReserveRequestDTO.builder()
-                .owner(2)
-                .guest(3)
-                .date("2023-02-08 12:00:00")
+//        ReserveRequestDTO requestDTO = ReserveRequestDTO.builder()
+//                .owner(2)
+//                .guest(3)
+//                .date("2023-02-08 12:00:00")
+//                .build();
+//        reserveService.addReserve(requestDTO);
+        String date = "2023-02-08 11:10:50";
+        User owner = User.builder()
+                .userIndex(3)
                 .build();
-        reserveService.addReserve(requestDTO);
+        User guest = User.builder()
+                .userIndex(4)
+                .build();
+        meetingRepository.existsMeeting(guest, owner,Timestamp.valueOf(date));
+
     }
 }
