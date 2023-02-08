1a483b38b751a3cce30b7261b62efe79473db16d
jaehe <jaehe0413@naver.com>
2023-02-08 17:28:30 +0900

    edit : 매물 설명회 등록, 조회

--- client/naega-galga/src/components/product/detail/ExplanationBox.vue
+++ client/naega-galga/src/components/product/detail/ExplanationBox.vue
@@ -6,11 +6,12 @@
     <div class="margin-top">
       <div class="flex">
         <!-- 설명회 정보-->
-        <div class="font-medium">
-          {{ explanationDateString }}
+        <div v-if="meetingInfo.meetingIndex > 0" class="font-medium">
+          {{ meetingInfo.explanationDate }}
         </div>
+        <div v-else>일정이 없습니다</div>
         <!-- 내가 등록한 매물인가 -->
-        <div v-if="isMine" class="flex-self">
+        <div class="flex-self">
           <el-button
             v-if="canAddExplanation"
             type="primary"
@@ -19,16 +20,14 @@
             @click="onClickAddExplanation"
           />
           <el-button
-            v-else
+            v-if="canDeleteExplanation"
             circle
             type="danger"
             :icon="Minus"
             class="button-size"
             @click="onClickDeleteExplanation"
           />
-        </div>
-        <!-- 내가 등록한 매물 아님 -->
-        <div v-if="!isMine && isRegisteredExplanation">
+          <!-- 내가 등록한 매물 아님 -->
           <el-button
             v-if="canAddReservation"
             circle
@@ -38,7 +37,7 @@
             @click="onClickCancelReserveExplanation"
           />
           <el-button
-            v-else
+            v-if="canDeleteReservation"
             type="danger"
             :icon="Minus"
             class="button-size"
@@ -55,11 +54,13 @@
 </template>
 
 <script lang="ts">
-import { ref, reactive, computed } from "vue";
+import { ref, reactive, computed, onUpdated } from "vue";
 import { ProductReservation } from "@/types/MeetingReservationType";
 import { Calendar, Minus, Plus } from "@element-plus/icons-vue";
 import { useStore } from "vuex";
 import ExplanationAddDialog from "@/components/product/detail/ExplanationAddDialog.vue";
+import { getExplanationInfo } from "@/api/explanationApi";
+import ResponseStatus from "@/api/responseStatus";
 
 export default {
   props: {
@@ -76,37 +77,71 @@ export default {
     const meetingInfo: ProductReservation = reactive({
       productIndex: productIndexRef.value,
       meetingIndex: -1,
-      explanationDate: "2022.02.01 11:00",
+      explanationDate: "일정이 없습니다",
       sellerIndex: -1,
       buyerIndex: null,
     });
 
-    //api로 meeting 정보 가져오는 부분 추가
+    const dateToFormatString = (dateString: string): string => {
+      const blankSplitlist = dateString.split(" ");
+      const dateSplitList = blankSplitlist[0].split("-");
+      const timeSplitList = blankSplitlist[1].split(":");
+
+      const nowDate = new Date();
+      const yearValue = parseInt(dateSplitList[0]);
+      //오늘이랑 다른 년도면 표시 아니면 표시 안함
+      const year = nowDate.getFullYear() !== yearValue ? `${yearValue}년` : "";
+
+      const month = parseInt(dateSplitList[1]);
+      const day = parseInt(dateSplitList[2]);
+
+      const hour = parseInt(timeSplitList[0]);
+      const minutes = parseInt(timeSplitList[1]);
+
+      const str = `${year} ${month}월 ${day}일 ${hour}시 ${minutes}분 예정`;
+      return str;
+    };
+
+    const getMeetingInfo = async () => {
+      const response = await getExplanationInfo(meetingInfo.productIndex);
+
+      if (response.status === ResponseStatus.NoContent) {
+        return;
+      }
+      if (response.status === ResponseStatus.InternalServerError) {
+        alert("서버 오류로 실행할 수 없습니다");
+      }
+      const data = response.data;
+      meetingInfo.meetingIndex = data.meetingIndex;
+      meetingInfo.sellerIndex = data.sellerIndex;
+      meetingInfo.buyerIndex = data.buyerIndex;
+      if (meetingInfo.explanationDate !== null) {
+        meetingInfo.explanationDate = dateToFormatString(data.reservedAt);
+      }
+    };
+    getMeetingInfo();
+
+    onUpdated(() => getMeetingInfo());
 
     const isMine = computed(() => myIndex.value === meetingInfo.sellerIndex);
 
-    //DB에 등록된 설명회가 있는지
-    const isRegisteredExplanation = computed(
-      () => meetingInfo.meetingIndex !== -1
-    );
     //내가 등록된 매물인데 설명회가 없는 경우 설명회 등록 가능
     const canAddExplanation = computed(
-      () => isMine.value && !isRegisteredExplanation.value
+      () => isMine.value && meetingInfo.meetingIndex <= 0
     );
 
-    const explanationDateString = computed(() =>
-      isRegisteredExplanation.value
-        ? meetingInfo.explanationDate
-        : "일정이 없습니다"
+    const canDeleteExplanation = computed(
+      () => isMine.value && meetingInfo.meetingIndex > 0
     );
 
-    //구매자일 경우 내가 예약 추가한 설명회인지
-    const isReserved = computed(
+    //구매자가 예약 삭제
+    const canDeleteReservation = computed(
       () => !isMine.value && meetingInfo.buyerIndex === myIndex.value
     );
 
+    //구매자가 예약 추가
     const canAddReservation = computed(
-      () => isRegisteredExplanation.value && !isReserved.value
+      () => !isMine.value && meetingInfo.buyerIndex !== myIndex.value
     );
 
     //click event
@@ -135,10 +170,10 @@ export default {
       //values
       meetingInfo,
       isMine,
-      isRegisteredExplanation,
       canAddExplanation,
-      explanationDateString,
+      canDeleteExplanation,
       canAddReservation,
+      canDeleteReservation,
       //click event
       onClickAddExplanation,
       dialogShow,
